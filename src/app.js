4c5de1b4e205049f497c90feb65e66b08414c598
zedbeit <devbeita@gmail.com>
2020-05-30 11:48:35 +0100

    add more packages

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const path = require('path');
 require('./db/mongoose');
 const pagesRouter = require('./routes/pages');
 const adminRouter = require('./routes/admin_pages');
-
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const port = 5000;
 
@@ -21,9 +22,25 @@ app.use(express.static(publicDir));
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
 
-// set routes
+// routes setup 
 app.use('/admin/pages', adminRouter);
 app.use('/', pagesRouter);
+
+// Body-parser middleware
+// 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Express-session middleware
+// 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
 
 app.listen(port, () => {
     console.log(`Server is up at port ${port}`);
