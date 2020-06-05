66b07c0dbf3104ca4df4e69530a04d3952491894
zedbeit <devbeita@gmail.com>
2020-06-05 13:33:18 +0100

    refactor: modify post route handler

--- src/routes/admin_pages.js
+++ src/routes/admin_pages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const Page = require('../models/pages');
 
 const router = express.Router();
 
@@ -27,7 +28,7 @@ router.post('/add-page', [
         .not().isEmpty(),
     check('content', 'Content must not be empty')
         .not().isEmpty()
-], (req, res) => {
+], async (req, res) => {
 
     const title = req.body.title;
     const slug = req.body.slug === '' ? title.replace(/\s+/g, '-').toLowerCase(): req.body.slug.replace(/\s+/g, '-').toLowerCase();
@@ -37,14 +38,77 @@ router.post('/add-page', [
     
     if(!errors.isEmpty()) {
         // return res.status(422).json({ errors: errors.array() });    
-        res.render('admin/add_page', {
+        return res.render('admin/add_page', {
             errors: errors.array(),
             title,
             slug,
             content
         });
     }
-    console.log(slug);
+
+    // Page.findOne({slug}, (err, page) => {
+    //     if (page) {
+    //         return res.render('admin/add_page', {
+    //             errors: [{msg: 'Page or Slug already exists!'}],
+    //             title,
+    //             slug,
+    //             content
+    //         });
+    //     }
+
+    //     page = new Page({
+    //         title,
+    //         slug,
+    //         content,
+    //         sorting: 0
+    //     });
+        
+    //     page.save((err) => {
+    //         if(err) {
+    //             return res.send('Page could not be saved!');
+    //         }
+    //         res.redirect('/admin/pages');
+    //     });
+
+    // })
+
+    try {
+        const slugExist = await Page.findOne({slug});
+
+        if(slugExist) {
+            return res.render('admin/add_page', {
+                errors: [{msg: 'Page or Slug already exists!'}],
+                title,
+                slug,
+                content
+            });
+        }
+
+        const page = new Page({
+            title,
+            slug,
+            content
+        });
+
+        await page.save();
+        
+        res.redirect('/admin/pages');
+
+
+    } catch (e) {
+        // return res.status(400).send(e);
+        return res.send('Page could not be saved!');
+    }
+    
+
+    // const page = await Page.findOne({slug})
+    
+    // if (page) {
+
+    // }
+    
+
+
     // res.send('Success!');
 
 });
