46ba5ed8cf8c2d46511f6a0ea24be7b68c459ba5
zedbeit <devbeita@gmail.com>
2020-06-18 11:00:27 +0100

    add category POST and GET routes

--- src/routes/admin_categories.js
+++ src/routes/admin_categories.js
@@ -4,7 +4,7 @@ const Category = require('../models/category');
 
 const router = express.Router();
 
-// Get pages index
+// GET pages index
 router.get('/', async (req, res) => { // */* =>  */admin/pages
     try {
         const categories = await    Category.find({});
@@ -18,65 +18,51 @@ router.get('/', async (req, res) => { // */* =>  */admin/pages
     }
 });
 
-// add page
-router.get('/add-page', (req, res) => { // */* =>  */admin/pages*
-
+// GET add page
+router.get('/add-category', (req, res) => { // */* =>  */admin/categories*
     const title = '';
-    const slug = '';
-    const content = '';
-
-    res.render('admin/add_page', {
-        title,
-        slug,
-        content
+    res.render('admin/add_category', {
+        title
     });
 });
 
-router.post('/add-page', [
+// POST add page
+router.post('/add-category', [
     check('title', 'Title value is empty')
-        .not().isEmpty(),
-    check('content', 'Content must not be empty')
         .not().isEmpty()
 ], async (req, res) => {
-
     const title = req.body.title;
-    const slug = req.body.slug === '' ? title.replace(/\s+/g, '-').toLowerCase(): req.body.slug.replace(/\s+/g, '-').toLowerCase();
-    const content = req.body.content;
+    const slug = title.replace(/\s+/g, '-').toLowerCase();
     
     const errors = validationResult(req);
     
     if(!errors.isEmpty()) {
         // return res.status(422).json({ errors: errors.array() });    
-        return res.render('admin/add_page', {
+        return res.render('admin/add_category', {
             errors: errors.array(),
-            title,
-            slug,
-            content
+            title
         });
     }
 
     try {
-        const slugExist = await Page.findOne({slug});
+        const slugExist = await Category.findOne({slug});
 
         if(slugExist) {
-            return res.render('admin/add_page', {
-                errors: [{msg: 'Page or Slug already exists!'}],
-                title,
-                slug,
-                content
+            return res.render('admin/add_category', {
+                errors: [{msg: 'Category already exists!'}],
+                title
             });
         }
-        const page = new Page({
+        const category = new Category({
             title,
-            slug,
-            content,
-            sorting: 100
+            slug
         });
-        await page.save();
-        res.redirect('/admin/pages');
+        
+        await category.save();
+        res.redirect('/admin/categories');
     } catch (e) {
         // return res.status(400).send(e);
-        return res.send('Page could not be saved!');
+        return res.send('Categories could not be saved!');
     }
 });
 
